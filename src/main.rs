2f941360e03e7c48843f74bff9d6e5b997188cc2
Anhar Miah <anharhussainmiah@googlemail.com>
2023-08-15 10:52:02 +0100

    added ui base + all rust deps

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 mod logo;
 
+use lazy_static::lazy_static;
+use std::env;
+use uuid::Uuid;
+
 const VERSION: &str = "0.1.0";
 
+lazy_static! {
+    static ref DBURL: String = self::load_key("DATABASE_URL");
+}
 
-fn main() {
+#[tokio::main]
+async fn main() {
     logo::draw(&VERSION);
+}
+
+fn load_key(k: &str) -> String {
+    return match env::var(k) {
+        Ok(v) => v,
+        Err(_) => Uuid::new_v4().to_string(),
+    };
 }
