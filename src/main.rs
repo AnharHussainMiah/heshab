d5d6d2561b032d6f0c4b953c78d2fe00b9d68aa2
Anhar Miah <anharhussainmiah@googlemail.com>
2023-08-15 13:11:07 +0100

    have got sqlx migrations hooked up

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use lazy_static::lazy_static;
 use std::env;
 use uuid::Uuid;
 use warp::Filter;
+use std::process;
+use sqlx::PgPool;
 
 const VERSION: &str = "0.1.0";
 
@@ -14,10 +16,18 @@ lazy_static! {
 #[tokio::main]
 async fn main() {
     logo::draw(&VERSION);
-    let public = warp::get().and(warp::fs::dir("public"));
-
-    println!("==> serving application on port 0.0.0.0:8080 use CTL+C to stop..");
-    warp::serve(public).run(([0, 0, 0, 0], 8080)).await;
+    if let Ok(pool) = PgPool::connect(&DBURL).await {
+        let _ = sqlx::migrate!().run(&pool).await;
+        
+        
+        let public = warp::get().and(warp::fs::dir("public"));
+        
+        println!("==> serving application on port 0.0.0.0:8080 use CTL+C to stop..");
+        warp::serve(public).run(([0, 0, 0, 0], 8080)).await;
+    } else {
+        println!("WARNING: unable to establish a database connection, exiting...");
+        process::exit(1);
+    }
 }
 
 fn load_key(k: &str) -> String {
