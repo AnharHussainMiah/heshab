7d1478334fe1c8a9133ca50040182b1af7c16291
Anhar Miah <anharhussainmiah@googlemail.com>
2023-08-15 17:30:10 +0100

    added bcrypt check

--- src/authenticate.rs
+++ src/authenticate.rs
@@ -1,4 +1,6 @@
+extern crate bcrypt;
 
+use bcrypt::{DEFAULT_COST, hash, verify};
 use crate::data;
 use crate::CompanyInfo;
 use serde::{Deserialize, Serialize};
@@ -6,11 +8,15 @@ use sqlx::PgPool;
 use warp::http::StatusCode;
 use warp::reply::{json, with_status};
 
-
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct AuthenticatePayload {
     email: String,
     password: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct TokenResponse {
+    token: String,
 }
 
 pub async fn handle(
@@ -19,8 +25,10 @@ pub async fn handle(
 ) -> Result<impl warp::Reply, warp::Rejection> {
     if let Ok(company) = data::get_company_by_email(&pool, &payload.email).await {
         println!("==> got back -> {:?}", company);
+        // compare hashes and if ok then sign the token and send back
+        if let Ok(is_valid) = verify(company.password, &payload.password) {
 
-        
+        }
     }
 
 
